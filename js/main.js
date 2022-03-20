da5a44b64948c23e44c7239ba863e761a40f00c1
atakriti <anwart256@gmail.com>
2022-03-20 14:04:39 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function shipping() {
+    let element = document.getElementById("shipping-container");
+    if (element.style.visibility == "hidden") {
+        element.style.visibility = "visible";       
+    } else {
+        element.style.visibility = "hidden";
+    }
+}
+// ============ Delivery =============
+function delivery() {
+    let element = document.getElementById("delivery-container");
+    if (element.style.visibility == "hidden") {
+        element.style.visibility = "visible";       
+    } else {
+        element.style.visibility = "hidden";
+    }
+}
